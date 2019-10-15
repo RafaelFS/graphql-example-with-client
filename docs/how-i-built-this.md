a064a71b27d693fae735d1b29842febf6e1d72b0
RafaelFS <rfl.fsilva@gmail.com>
2019-10-15 00:27:05 -0300

    Add components and display list with mock data

--- docs/how-i-built-this.md
+++ docs/how-i-built-this.md
@@ -9,3 +9,17 @@ The goal of this document is to mainly help new people follow along my implement
 The first step was simply to run Create React App on my machine. Follow the instructions on https://github.com/facebook/create-react-app, it is much better explained.
 
 ![Image showing the result of create react app: the react logo with the text: Edit src/App.js and save to reload.](./assets/step1.png)
+
+
+## Importing components
+For the next step I copied over some components from my old repository https://github.com/RafaelFS/graphql-presentation. 
+
+I won't go over the details on how they were created, but they form a simple grid of characters cards. You can find plenty of examples on how to do similar designs on https://codepen.io/. 
+
+I chose to keep them in plain html and using css classes (no Styled Components) so it is easier to setup and folllow along.
+
+## Mocking data
+Alongside importing the old components, I also mocked some data to populate them. The images were selected by browsing the [Rick and Morty API](https://rickandmortyapi.com/) - more on that later.
+
+![Image showing the results after the previous steps: two cards showing Rick and Morty from the popular TV show.](./assets/step2.png)
+
--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
+import './CharacterCard.css';
 import './App.css';
+
+const characters = [
+  {
+    name: 'Rick',
+    species: 'Human',
+    image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg'
+  },
+  {
+    name: 'Morty',
+    species: 'Human',
+    image: 'https://rickandmortyapi.com/api/character/avatar/2.jpeg'
+  }
+]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CharacterList characters={characters}/>
+    </div>
+  );
+}
+
+export function CharacterList(props) {
+  const characters = props.characters;
+  const cardStyle = {
+    marginTop: "50px",
+    marginRight: "15px"
+  }
+  const gridStyle = {
+    width: "100%",
+    display: "flex",
+    flexWrap: "wrap"
+  }
+  const listCharacters = characters.map(character =>
+    <div key={character.id} style={cardStyle}>
+      <CharacterCard style={cardStyle} character={character}/>
+    </div>
+  );
+  return (
+    <div style={gridStyle}>{listCharacters}</div>
+  );
+}
+
+function CharacterCard(props) {
+  let { name, image, species} = props.character;
+  return (
+    <div className="card">
+      <img src={image} alt="Avatar" className="card__img" />
+      <div className="card__container">
+        <h4>{name}</h4>
+        <p>{species}</p>
+      </div>
     </div>
   );
 }
